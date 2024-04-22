abf0cf20deb620be302694c307690e9781e8ea19
Dada Thimmy <oluwatimileyin962@gmail.com>
2024-04-22 16:01:52 +0000

    Finish custom template tags

--- blango/urls.py
+++ blango/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # other patterns
     path("", blog.views.index),
     path('admin/', admin.site.urls),
+        path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
 ]
